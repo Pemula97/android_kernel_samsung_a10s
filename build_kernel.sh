4909f2e88d471713c3714ee0abd4f1bfce945aeb
pemula <pemula@ubuntu.com>
2025-09-17 04:02:44 +0700

    Initial A107FXXU7BTK1

---
+++ build_kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PATH=$(pwd)/toolchain/clang-r353983c/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export KBUILD_BUILD_USER=Windows
+export KBUILD_BUILD_HOST=Ubuntu-Wsl
+export ARCH=arm64
+export SUBARCH=arm64
+
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+JOBS=$(nproc --all)
+
+echo "chek PATH..."
+which clang
+
+make -C $(pwd) O=$(pwd)/out CC=clang KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a10s_defconfig
+make -C $(pwd) O=$(pwd)/out CC=clang KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$JOBS 2>&1 | tee build.log
+
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
